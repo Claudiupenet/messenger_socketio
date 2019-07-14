c6858b78a36486c1a2010c5964187cebbc54881d
Claudiu <claudiupenet@gmail.com>
2019-07-14 17:33:37 +0300

    s

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const parser = require('body-parser');
 require("dotenv").config()
 
 
-const CONFIG = require('./config');
+const CONFIG = require('./app/config');
 const path = require("path");
 const port = process.env.PORT || CONFIG.PORT;
 
 const app = express();
 app.use(cors());
 app.use(parser.json())
-app.use(express.static('../client/build'));
+app.use(express.static('client/build'));
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve("../",__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const server = http.createServer(app);
 server.listen(port);
 io = socketIo(server);
 
-require('./controller/users')(io);
+require('./app/controller/users')(io);
