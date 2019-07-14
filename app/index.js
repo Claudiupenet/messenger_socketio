53ac4abe01e766c3d1e8be00dd06ee6cb0a34498
Claudiu <claudiupenet@gmail.com>
2019-07-14 16:52:28 +0300

    s\

--- app/index.js
+++ app/index.js
@@ -7,15 +7,15 @@ require("dotenv").config()
 
 
 const CONFIG = require('./config');
-const path = require("path")
+const path = require("path");
 const port = process.env.PORT || CONFIG.PORT;
 
 const app = express();
 app.use(cors());
 app.use(parser.json())
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static('client/build'));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const server = http.createServer(app);
